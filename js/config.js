98ce2230c6225c1712ead19b8d963a1f5d85ba8d
xingrach <167546540+xingrach@users.noreply.github.com>
2026-06-27 11:26:59 -0400

    Change Cognito user pool settings in config.js
    Updated AWS Cognito configuration values.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_VCUiInLki', // e.g. us-east-2_DDI13dkjes
+        userPoolClientId: 'j2ktpsl9dnske25579du8sij7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
